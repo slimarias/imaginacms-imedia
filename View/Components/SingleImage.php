2205eae9baf741ab55bdffc6e36c271f9333e303
Juan Escalante <jcec007@gmail.com>
2021-01-26 11:25:35 -0500

    +Update: make image with configurable

--- View/Components/SingleImage.php
+++ View/Components/SingleImage.php
@@ -23,16 +23,18 @@ class SingleImage extends Component
   public $mediumSrc;
   public $smallSrc;
   public $imgClasses;
+  public $with;
   
   public function __construct($src = '', $alt = '', $title = null, $url = null, $isMedia = false, $mediaFiles = null,
                               $zone = 'mainimage', $extraLargeSrc = null, $largeSrc = null, $mediumSrc = null,
-                              $smallSrc = null, $fallback = null, $imgClasses = '')
+                              $smallSrc = null, $fallback = null, $imgClasses = '', $with = "300px")
   {
     $this->src = $src;
     $this->alt = $alt;
     $this->title = $title;
     $this->url = $url;
     $this->imgClasses = $imgClasses;
+    $this->with = $with;
   
     if (!empty($fallback)) {
       $this->extension = pathinfo($fallback, PATHINFO_EXTENSION);
